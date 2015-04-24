8cf87cdcfaa6ddb61c005f1a51469062fcbfd2a9
Giovanni Lodi <giovanni.lodi42@gmail.com>
2015-04-24 17:30:46 +1000

    Update watch.js script to ignore .swp files

--- metallo/watch.js
+++ metallo/watch.js
@@ -6,13 +6,21 @@ function puts(error, stdout, stderr) {
 	if (stderr) { console.log(stderr); }
 }
 
+var anti_filter = function(pattern, fn) {
+  return function(filename) {
+    if (!pattern.test(filename)) {
+      fn(filename);
+    }
+  }
+}
+
 var baseDir = process.cwd() + "/metallo";
 var watched = [
 	baseDir + "/src",
 	baseDir + "/build.js",
 	baseDir + "/../package.json"
 	];
-watch(watched, function(filename) {
+watch(watched, anti_filter(/\.swp$/, function(filename) {
 	console.log(filename, 'changed. Re-buildind...');
 	exec("node " + baseDir + "/build.js", puts);
-});
+}));
